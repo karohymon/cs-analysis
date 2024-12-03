0ce4a17d32c94f73d3a78f620c12c4edf5350748
Karolin Hymon <khymon@cobalt-10.icecube.wisc.edu>
2024-12-02 21:33:49 -0600

    add options to change energy binning

--- csmodification_analyzer.py
+++ csmodification_analyzer.py
@@ -30,10 +30,23 @@ class SensitivityAnalyzer:
         self.doys = doys
         self.cs_mod =  self.ptype + '_' + str(self.scale_factor_p) + 'pion_' + str(self.scale_factor_k) + 'kaon_' +  str(self.threshold) + '_' + self.increase
                
+    def ebins_gev(self):
+        #create 1 bin but keep list format for compatibility with the other functions - for analysis of GeV muons
+        ebins = np.logspace(0, 1, num=2)
+        return ebins
+    
+    def ebins_rate(self):
+        #create 1 bin but keep list format for compatibility with the other functions - rate for detections in icecube
+        ebins = np.logspace(2, 6
+                            , num=2)
+        return ebins
+    
+    
     def ebins_analysis(self):
         step = 0.3
         ebins = np.logspace(2.1, 6, num=int((6 - 2.1) / step) + 1)
         return ebins
+
 
     def energybins_mceq(self):
         mceq = MCEqRun(
@@ -51,7 +64,7 @@ class SensitivityAnalyzer:
         if self.increase == 'const':
             self.flux_untuned = np.load(f'/data/user/khymon/cs-analysis/{self.ptype}_1.0pion_1.0kaon_{self.threshold}_{self.increase}_mceqflux.npy')   
         elif self.increase == 'exp':
-            self.flux_untuned = np.load(f'/data/user/khymon/cs-analysis/{self.ptype}_0.0pion_0.0kaon_{self.threshold}_{self.increase}git_mceqflux.npy')
+            self.flux_untuned = np.load(f'/data/user/khymon/cs-analysis/{self.ptype}_0.0pion_0.0kaon_{self.threshold}_{self.increase}_mceqflux.npy')
          
 
     def get_sv_amplitude(self, flux, energy, ebins, angles_edges, doys):
@@ -70,10 +83,10 @@ class SensitivityAnalyzer:
 
         return sv_amplitude
 
-    def get_amplitudes(self):
+    def get_amplitudes(self,ebins):
         angles_edges = angular_bins(self.ptype, 2)
         energy = self.energybins_mceq()
-        ebins = self.ebins_analysis()
+        #ebins = self.ebins_analysis()
 
         self.sv_amplitude_tuned = self.get_sv_amplitude(self.flux_tuned, energy, ebins, angles_edges, self.doys)
         self.sv_amplitude_untuned = self.get_sv_amplitude(self.flux_untuned, energy, ebins, angles_edges, self.doys)
@@ -81,34 +94,52 @@ class SensitivityAnalyzer:
 
     def deviation_sv_amplitude_plot(self):
         angles_edges =  angular_bins(self.ptype, 2)
-        ebins = self.ebins_analysis()
+     
+        energy = self.energybins_mceq()
+      
 
-        overall_deviation = np.ndarray(shape=(len(angles_edges) - 1, len(ebins)), dtype=float)
-        sensitivity = self.sv_amplitude_tuned / self.sv_amplitude_untuned
+        tag = ['allbins', 'gev', 'rate']
+        for e in range(3):
+            if e ==0 :
+                ebins = self.ebins_analysis()
+            if e ==1: 
+                ebins = self.ebins_gev()
+            elif e==2:
+                ebins = self.ebins_rate()
 
-        for j in range(len(angles_edges) - 1):
-            for i in range(len(ebins) - 1):
-                overall_deviation[j][i] = np.sum(np.abs(sensitivity[j][i] - 1))
+            self.sv_amplitude_tuned = self.get_sv_amplitude(self.flux_tuned, energy, ebins, angles_edges, self.doys)
+            self.sv_amplitude_untuned = self.get_sv_amplitude(self.flux_untuned, energy, ebins, angles_edges, self.doys)
 
-        plt.figure(figsize=(12, 6))
-        for i in range(len(ebins) - 4):
-            plt.plot(self.doys, sensitivity[0][i], marker='.', linestyle='-', label=f"{np.round(np.log10(ebins[i]), decimals=1)} $\leq$ log(E/GeV) $\leq$ {np.round(np.log10(ebins[i + 1]), decimals=1)}")
-        plt.axhline(y=1, color='black', linestyle='--', label='Original Value')
-        plt.title('Daily Sensitivity Across the Year')
-        plt.ylabel('Daily deviation from seasonal variation amplitude')
-        if self.ptype == 'numu':
-            plt.ylim(0.99,1.01)
-        elif self.ptype == 'mu':
-            plt.ylim(0.99,1.01)
-        else:
-            print('particle type is not defined.')
-            exit()
-        plt.xlabel('Day of Year')
-        plt.xlim()
-        plt.legend(ncol=2)
-        plt.grid(True)
-        plt.text(50,0.99,f'{self.ptype} {self.scale_factor_k}xkaon {self.scale_factor_p}pion threshold{self.threshold}GeV')
-        plt.savefig(f'/home/khymon/Plots/cs-analysis/sv_amplitude_mcs_allbins_dailysensitivity{self.cs_mod}_zenith{np.round(angles_edges[0], decimals=0)}-{np.round(angles_edges[1], decimals=0)}.png', bbox_inches='tight')
+            overall_deviation = np.ndarray(shape=(len(angles_edges) - 1, len(ebins)), dtype=float)
+            sensitivity = self.sv_amplitude_tuned / self.sv_amplitude_untuned # change this line
+
+            for j in range(len(angles_edges) - 1):
+                for i in range(len(ebins) - 1):
+                    overall_deviation[j][i] = np.sum(np.abs(sensitivity[j][i] - 1))
+
+            plt.figure(figsize=(12, 6))
+            if e == 0:
+                plotlooplength = len(ebins) - 4
+            else:
+                plotlooplength = len(ebins) -1
+            for i in range(plotlooplength):
+                plt.plot(self.doys, sensitivity[0][i], marker='.', linestyle='-', label=f"{np.round(np.log10(ebins[i]), decimals=1)} $\leq$ log(E/GeV) $\leq$ {np.round(np.log10(ebins[i + 1]), decimals=1)}")
+            plt.axhline(y=1, color='black', linestyle='--', label='Original Value')
+            plt.title('Daily Sensitivity Across the Year')
+            plt.ylabel('Daily deviation from seasonal variation amplitude')
+            if self.ptype == 'numu':
+                plt.ylim(0.99,1.01)
+            elif self.ptype == 'mu':
+                plt.ylim(0.99,1.01)
+            else:
+                print('particle type is not defined.')
+                exit()
+            plt.xlabel('Day of Year')
+            plt.xlim()
+            plt.legend(ncol=2)
+            plt.grid(True)
+            plt.text(50,0.99,f'{self.ptype} {self.scale_factor_k}xkaon {self.scale_factor_p}pion threshold{self.threshold}GeV')
+            plt.savefig(f'/home/khymon/Plots/cs-analysis/sv_amplitude_mcs_' + tag[e] +'_dailysensitivity{self.cs_mod}_zenith{np.round(angles_edges[0], decimals=0)}-{np.round(angles_edges[1], decimals=0)}.png', bbox_inches='tight')
 
     def sv_amplitude_plot(self):
         angles_edges  = angular_bins(self.ptype, 2)
