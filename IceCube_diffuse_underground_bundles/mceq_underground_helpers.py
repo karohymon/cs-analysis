0f79f8192ae4adf190e44ca2a300687c8e5a8128
karohymon <khymon@gate.sinica.edu.tw>
2024-12-16 12:16:27 +0800

    add flux for iron, not only yields

--- IceCube_diffuse_underground_bundles/mceq_underground_helpers.py
+++ IceCube_diffuse_underground_bundles/mceq_underground_helpers.py
@@ -319,6 +319,15 @@ def _flux(angle, flux_label, iecr=None):
     elif flux_label == 'jul_p15':
         return intp_surface_flux_GSF_jul_p15(cth)[:dim_ug]
     
+    elif flux_label == 'jan_p15_fe':
+        return intp_surface_flux_GSF_jan_p15_fe(cth)[:dim_ug]
+    
+    elif flux_label == 'apr_p15_fe':
+        return intp_surface_flux_GSF_apr_p15_fe(cth)[:dim_ug]
+    
+    elif flux_label == 'jul_p15_fe':
+        return intp_surface_flux_GSF_jul_p15_fe(cth)[:dim_ug]
+    
     elif flux_label == "yields_jan":
         assert iecr is not None
         return intp_ground_mu_yields_jan[iecr](cth)[:dim_ug]
