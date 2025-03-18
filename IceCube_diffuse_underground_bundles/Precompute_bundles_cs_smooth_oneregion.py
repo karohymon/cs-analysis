corect e0 and e1 loop, correct angles to match 0 as first angle for float point precision

IceCube_diffuse_underground_bundles/Precompute_bundles_cs_smooth_oneregion.py
@@ -68,7 +68,7 @@ def main(scale_factor_p, scale_factor_k, e0,e1, increase,interactionmodel,nucleu
     mceq_air.set_interaction_model("SIBYLL23C", force=True)
     mceq_air.set_theta_deg(0)
 
-    cos_thetas = np.arange(0.5, 1.001, 0.1)
+    cos_thetas = np.linspace(0.5, 1.0, num=6)
     thetas = np.degrees(np.arccos(cos_thetas))
 
     # modify cross section
@@ -132,22 +132,38 @@ def main(scale_factor_p, scale_factor_k, e0,e1, increase,interactionmodel,nucleu
 
     # adapt file names
     if e1 is None:
+        if scale_factor_p == 1.0 and scale_factor_k == 1.0:
+            pickle.dump(
+                [mceq_air.e_grid, cos_thetas, cr_grid, ground_muspec_prim_energies[0], ground_muspec_prim_energies[1], ground_muspec_prim_energies[2]],
+                open(f"/hetghome/khymon/cs-files/smooth-transition/ground_muspec_prim_energies_season_cstune{nucleus}"
+                    f"_pi{float(scale_factor_p):.2f}"
+                    f"_k{float(scale_factor_k):.2f}.pkl", "wb"),
+                    
+            )
+            pickle.dump(
+                [mceq_air.e_grid, cos_thetas, surface_flux_GSF[0], surface_flux_GSF[1], surface_flux_GSF[2]],
+                open(f"/hetghome/khymon/cs-files/smooth-transition/surface_fluxes_season{nucleus}"
+                    f"_pi{float(scale_factor_p):.2f}"
+                    f"_k{float(scale_factor_k):.2f}.pkl", "wb"),
+            )
 
-        pickle.dump(
-            [mceq_air.e_grid, cos_thetas, cr_grid, ground_muspec_prim_energies[0], ground_muspec_prim_energies[1], ground_muspec_prim_energies[2]],
-            open(f"/hetghome/khymon/cs-files/smooth-transition/ground_muspec_prim_energies_season_cstune{nucleus}"
-                f"_pi{float(scale_factor_p):.2f}"
-                f"_k{float(scale_factor_k):.2f}"
-                f"_e0{float(e0):.2f}_{increase}.pkl", "wb"),
-                
-        )
-        pickle.dump(
-            [mceq_air.e_grid, cos_thetas, surface_flux_GSF[0], surface_flux_GSF[1], surface_flux_GSF[2]],
-            open(f"/hetghome/khymon/cs-files/smooth-transition/surface_fluxes_season{nucleus}"
-                f"_pi{float(scale_factor_p):.2f}"
-                f"_k{float(scale_factor_k):.2f}"
-                f"_e0{float(e0):.2f}_{increase}.pkl", "wb"),
-        )
+        else:
+            pickle.dump(
+                [mceq_air.e_grid, cos_thetas, cr_grid, ground_muspec_prim_energies[0], ground_muspec_prim_energies[1], ground_muspec_prim_energies[2]],
+                open(f"/hetghome/khymon/cs-files/smooth-transition/ground_muspec_prim_energies_season_cstune{nucleus}"
+                    f"_pi{float(scale_factor_p):.2f}"
+                    f"_k{float(scale_factor_k):.2f}"
+                    f"_e0{float(e0):.2f}_{increase}.pkl", "wb"),
+            
+                    
+            )
+            pickle.dump(
+                [mceq_air.e_grid, cos_thetas, surface_flux_GSF[0], surface_flux_GSF[1], surface_flux_GSF[2]],
+                open(f"/hetghome/khymon/cs-files/smooth-transition/surface_fluxes_season{nucleus}"
+                    f"_pi{float(scale_factor_p):.2f}"
+                    f"_k{float(scale_factor_k):.2f}"
+                    f"_e0{float(e0):.2f}_{increase}.pkl", "wb"),
+            )
     else: 
         pickle.dump(
             [mceq_air.e_grid, cos_thetas, cr_grid, ground_muspec_prim_energies[0], ground_muspec_prim_energies[1], ground_muspec_prim_energies[2]],

IceCube_diffuse_underground_bundles/R_generic_oneregion.py
@@ -16,7 +16,7 @@ def X(d):
     ''' calculate slant depth ad a given vertical depth in km'''
     s_d = mh.slant_depths
     angles = mh.angles
- 
+     
     return d/np.cos(np.deg2rad(angles))
 
 def dNmu_dmu(d,month, ptype, cs_p, cs_k, e0,e1=None): # month = str
@@ -140,8 +140,8 @@ def main(calculation,normalization):
         cs_k_values = [1.00]
         ptype_values = [2212] 
         season_values = ["jan", "apr", "jul"]  #  seasons
-        e0_values = [2.05,2.15,2.25,2.35,2.45,2.55,2.65,2.75,2.85,2.95,3.05,3.15,3.25,3.35,3.45,3.55,3.65,3.75,3.85,3.95,4.05,4.15,4.25,4.35,4.45,4.55,4.65,4.75,4.85]
-        e1_values = [2.25,2.35,2.45,2.55,2.65,2.75,2.85,2.95,3.05,3.15,3.25,3.35,3.45,3.55,3.65,3.75,3.85,3.95,4.05,4.15,4.25,4.35,4.45,4.55,4.65,4.75,4.85,4.95,5.05]
+        e0_values = [2.05]#,2.15,2.25,2.35,2.45,2.55,2.65,2.75,2.85,2.95,3.05,3.15,3.25,3.35,3.45,3.55,3.65,3.75,3.85,3.95,4.05,4.15,4.25,4.35,4.45,4.55,4.65,4.75,4.85]
+        e1_values = [2.25]#,2.35,2.45,2.55,2.65,2.75,2.85,2.95,3.05,3.15,3.25,3.35,3.45,3.55,3.65,3.75,3.85,3.95,4.05,4.15,4.25,4.35,4.45,4.55,4.65,4.75,4.85,4.95,5.05]
         pairwise = True
 
     elif calc_tag =='energybin_test_kaon':
@@ -170,25 +170,23 @@ def main(calculation,normalization):
 
     for d in d_values:
         x_mod = X(d)  # for specific depth
-
+        
         for cs_p in cs_p_values:
             for cs_k in cs_k_values:
                 for ptype in ptype_values:
                     for season in season_values:
                         if cs_p == 1.0:
                             # Special case when cs_p = 1.0: use 'inf' for e1
-                            for e0 in e0_values:
-                                # Compute R with special handling for cs_p=1.0
-                                dNmu_dmu_mod = dNmu_dmu(d, season, ptype, cs_p, cs_k, e0)
-                                R_mod = R(m, dNmu_dmu_mod)
-                                if calc_tag == 'threshold':
-                                    R_norm = R_normalized_threshold(m, R_mod, d, ptype, e0)
-                                    print('e0 changed in normalization of R')
-                                else:
-                                    R_norm = R_normalized(m, R_mod, d, ptype)
-
-                                # Store the result in the dictionary
-                                results[(str(d), str(cs_p), str(cs_k), str(ptype), season, str(e0), "inf")] = R_norm if normalization else R_mod
+                            e0 = e0_values[0]  # Assign a default value for accessing the keys correctly
+                            # Compute R with special handling for cs_p=1.0
+                            dNmu_dmu_mod = dNmu_dmu(d, season, ptype, cs_p, cs_k, e0)
+                            R_mod = R(m, dNmu_dmu_mod)
+                            if calc_tag == 'threshold':
+                                R_norm = R_normalized_threshold(m, R_mod, d, ptype, e0)
+                                print('e0 changed in normalization of R')
+                            
+                            # Store the result in the dictionary
+                            results[(str(d), str(cs_p), str(cs_k), str(ptype), season, str(e0), "inf")] = R_norm if normalization else R_mod
                         else:
                             # For other cs_p values: handle both pairwise and non-pairwise combinations of e0 and e1
                             if e1_values is not None:
@@ -199,9 +197,7 @@ def main(calculation,normalization):
                                     if calc_tag == 'threshold':
                                         R_norm = R_normalized_threshold(m, R_mod, d, ptype, e0)
                                         print('e0 changed in normalization of R')
-                                    else:
-                                        R_norm = R_normalized(m, R_mod, d, ptype)
-
+                                    
                                     # Store the result in the dictionary
                                     results[(str(d), str(cs_p), str(cs_k), str(ptype), season, str(e0), str(e1))] = R_norm if normalization else R_mod
                             else:
@@ -214,9 +210,7 @@ def main(calculation,normalization):
                                         if calc_tag == 'threshold':
                                             R_norm = R_normalized_threshold(m, R_mod, d, ptype, e0)
                                             print('e0 changed in normalization of R')
-                                        else:
-                                            R_norm = R_normalized(m, R_mod, d, ptype)
-
+                                        
                                         # Store the result in the dictionary
                                         results[(str(d), str(cs_p), str(cs_k), str(ptype), season, str(e0), "inf")] = R_norm if normalization else R_mod
 

IceCube_diffuse_underground_bundles/mceq_underground_helpers_oneregion.py
@@ -44,17 +44,23 @@ def initialize_flux_dicts(ptype_values, cs_p_values, cs_k_values, e0_values, e1_
             for cs_k in cs_k_values:
                 if cs_p == 1.0:
                     # Special case when cs_p is 1.0: only use one specific combination of e0 and e1 = None
-                    #for e0 in e0_values: #only one e0 needed technically
-                    
-                        flux_files[(ptype, round(cs_p, 2), round(cs_k, 2), round(e0, 2), "inf")] = \
-                            cs_dir / f"surface_fluxes_season{ptype}_pi{cs_p:.2f}_k{cs_k:.2f}_e0{e0:.2f}_const.pkl"
+                    #for e0 in range(len(e0)): #only one e0 needed technically
+
+                    e0 = 2.05#e0_values[0]  # Assign a default value for accessing the keys correctly
                         
-                        muspec_files[(ptype, round(cs_p, 2), round(cs_k, 2), round(e0, 2), "inf")] = \
-                            cs_dir / f"ground_muspec_prim_energies_season_cstune{ptype}_pi{cs_p:.2f}_k{cs_k:.2f}_e0{e0:.2f}_const.pkl"
+                    flux_files[(ptype, round(cs_p, 2), round(cs_k, 2), round(e0, 2), "inf")] = \
+                        cs_dir / f"surface_fluxes_season{ptype}_pi{cs_p:.2f}_k{cs_k:.2f}.pkl"#_e0{e0:.2f}_const.pkl"
+                        
+                    muspec_files[(ptype, round(cs_p, 2), round(cs_k, 2), round(e0, 2), "inf")] = \
+                        cs_dir / f"ground_muspec_prim_energies_season_cstune{ptype}_pi{cs_p:.2f}_k{cs_k:.2f}.pkl"#_e0{e0:.2f}_const.pkl"
                 else:
                     # Handle case where pairwise is True
                     if pairwise:
                         for e0, e1 in zip(e0_values, e1_list):  # Pair e0 and e1 element-wise
+                            e0 = float(e0)  # Convert NumPy scalar to float
+                            e1 = float(e1)  # Convert e1 as well if needed
+
+                                                        
                             flux_files[(ptype, round(cs_p, 2), round(cs_k, 2), round(e0, 2), round(e1, 2) if e1 is not None else "inf")] = \
                                 cs_dir / f"surface_fluxes_season{ptype}_pi{cs_p:.2f}_k{cs_k:.2f}_e0{e0:.2f}_const_{'inf' if e1 is None else f'e1{e1:.2f}'}.pkl"
                             
@@ -98,15 +104,7 @@ def initialize_flux_dicts(ptype_values, cs_p_values, cs_k_values, e0_values, e1_
                 "apr": np.asarray(fluxes[1]).swapaxes(0, 1),
                 "jul": np.asarray(fluxes[2]).swapaxes(0, 1),
             }
-    print(cos_thetas.shape)
-
-    for key in surface_fluxes:
-        for season, flux in surface_fluxes[key].items():
-            print(f"{key} - {season}:")
-            print(f"Length of cos_thetas: {len(cos_thetas)}")
-            print(f"Shape of flux: {flux.shape}")  # Print full shape to check dimensions
-            print("---")
-
+       
     # Create interpolators for surface fluxes
     intp_surface_fluxes = {
         key: {
@@ -119,7 +117,7 @@ def initialize_flux_dicts(ptype_values, cs_p_values, cs_k_values, e0_values, e1_
     # Create interpolators for ground mu yields
     intp_ground_mu_yields = {
         key: {
-            season: [
+            season: [ 
                 ip.interp1d(cos_thetas, ground_muspec_energies[key][season][:, ie, :], axis=0, kind="linear")
                 for ie in range(len(cr_grid))
             ]
@@ -133,6 +131,7 @@ def initialize_flux_dicts(ptype_values, cs_p_values, cs_k_values, e0_values, e1_
     # Now calculate angles and c_wi globally
     angles = np.degrees(np.arccos(cos_thetas))  # Convert from cos(theta) to theta in degrees
     c_wi = np.diff(cos_thetas)  # Calculate the differential width in cos(theta)
+
 def some_function_that_uses_angles():
     global angles
     if angles is None:
@@ -156,6 +155,8 @@ _X_MIN = 0.5
 _X_MAX = 14
 
 _SLANT_DEPTHS = np.linspace(_X_MIN, _X_MAX, int(2 * (_X_MAX - _X_MIN) + 1))
+#step = 0.5  # Since 2 * (X_MAX - X_MIN) + 1 suggests 0.5 spacing
+#_SLANT_DEPTHS = np.arange(_X_MIN, _X_MAX + step, step)
 # _ANGLES = np.degrees(np.arccos(_X_MIN / _SLANT_DEPTHS))
 
 
@@ -242,8 +243,17 @@ def _flux(angle, flux_label, ptype=2212, cs_p=1.0, cs_k=1.0, e0 =1000.0, e1=None
     Returns:
         numpy.ndarray: Flux.
     """
+
     cth = np.cos(np.radians(angle))
-    assert np.min(cth) >= cos_thetas[0] and np.max(cth) <= cos_thetas[-1]
+
+    # Directly set the maximum value to 1.0 for cos(theta)
+    cos_thetas_max_adjusted = 1.0  # Explicitly set the upper bound to 1.0
+
+    # Check that the min and max of cth are within the bounds of cos_thetas
+    assert np.min(cth) >= cos_thetas[0] and np.max(cth) <= cos_thetas_max_adjusted
+
+
+
 
     if cs_p == 1.0:
         e1 = 'inf'  # Explicitly set e1 to 'inf' when cs_p = 1.0