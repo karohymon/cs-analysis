d450422ee75be74a56d4b0b23c01878c4541a6e4
karohymon <khymon@gate.sinica.edu.tw>
2025-06-10 12:00:04 +0800

    clean up repo

---
+++ IceCube_diffuse_underground_bundles/old_dev_scripts/Precompute_bundles_cs.py
@@ -0,0 +1,138 @@
+import click
+import numpy as np
+import matplotlib.pyplot as plt
+from matplotlib import cm
+from matplotlib.colors import Normalize
+import matplotlib.lines as mlines
+from scipy.interpolate import UnivariateSpline
+from tqdm import tqdm
+from MCEq.core import MCEqRun
+import mceq_config as config
+import crflux.models as pm
+from MCEq.data import InteractionCrossSections
+import sys
+import os
+import pickle
+
+#os.chdir('/hetghome/khymon/cs-analysis/')
+parent_dir = os.path.abspath(os.path.join(os.getcwd(), '..'))
+sys.path.insert(0, parent_dir)
+# Import the class
+from cs_modifier import ModIntCrossSections
+
+
+@click.command()
+@click.option('--scale_factor_k','-s', help='list with cs modification kaon, 1 = untuned for const, 0 = untuned for exp')
+@click.option('--scale_factor_p','-f', help='list with cs modification pion, 1 = untuned for const, 0 = untuned for exp')
+@click.option('--threshold','-t', help='threshold above which modification is applied', default='1.e4')
+@click.option('--increase','-i', help='const or exp', default='const')
+@click.option('--interactionmodel','-m', help='hadr. interaction model', default="SIBYLL2.3c")
+@click.option('--nucleus','-n', help='proton: 2212 or iron: 5626', default="2212")
+
+
+def main(scale_factor_p, scale_factor_k, threshold,increase,interactionmodel,nucleus):
+
+
+    scale_factor_p = float(f'{scale_factor_p}')
+    scale_factor_k = float(f'{scale_factor_k}')
+    threshold = float(f'{threshold}')
+    increase = f'{increase}'
+    nucleus = int(f'{nucleus}')
+
+    scale_factor = [scale_factor_p,scale_factor_k]
+
+
+    mceq_air = MCEqRun(
+        interaction_model="SIBYLL23C",
+        theta_deg=0.0,
+        primary_model=(pm.GlobalSplineFitBeta, None),
+        density_model = ('MSIS00_IC',('South Pole','January')),
+    )
+
+    mceq_air.set_interaction_model("SIBYLL23C", force=True)
+    mceq_air.set_theta_deg(0)
+
+    cos_thetas = np.arange(0.8, 1.001, 0.1)
+    thetas = np.degrees(np.arccos(cos_thetas))
+
+    #initialize mceq instances
+    mceq_tune = MCEqRun(
+        interaction_model="SIBYLL23C",
+        theta_deg=0.0,
+        primary_model=(pm.GlobalSplineFitBeta, None),
+        density_model = (('MSIS00_IC',('SouthPole','January')))
+    )
+
+
+    # modify cross section
+    modcs = ModIntCrossSections(mceq_air._mceq_db, interaction_model=interactionmodel, scale_factor=scale_factor,threshold=threshold,increase=increase) #scale_factor=[1.,1.3],threshold=1.e4,increase='const')
+    modcs.load(interaction_model=interactionmodel)
+
+    mceq_tune._int_cs = modcs # add modification to cross section in mceq instance
+    mceq_tune.set_interaction_model(interactionmodel, force=True) # necessary to force cross section change
+
+    #test if tuning is correct:
+    print('ratio pion cross section tuned/untuned: ', modcs.get_cs(211, mbarn=True)/InteractionCrossSections(mceq_air._mceq_db, interaction_model=interactionmodel).get_cs(211, mbarn=True))
+    print('ratio kaon cross section tuned/untuned: ', modcs.get_cs(321, mbarn=True)/InteractionCrossSections(mceq_air._mceq_db, interaction_model=interactionmodel).get_cs(321, mbarn=True))
+
+    # calcualte tuned flux for different seasons
+    seasons = ['January', 'April', 'July']
+    surface_flux_GSF = []
+
+    for season in tqdm(seasons):  # Loop over seasons
+        season_flux = []  # Temporary container for the current season's results
+        for ia, theta in enumerate(thetas):  # Loop over angles
+            mceq_tune.set_theta_deg(theta)
+            
+            # Optionally set the season-dependent atmospheric model here
+            mceq_tune.density_model.set_season(season)  # Uncomment and adjust if applicable
+            
+            # Solve for the current angle and season
+            mceq_tune.solve()
+            
+            # Append the solution for the current angle
+            season_flux.append(
+                mceq_tune.get_solution("mu+", mag=0) + mceq_tune.get_solution("mu-", mag=0)
+            )
+        # Append the results for the current season
+        surface_flux_GSF.append(season_flux)
+
+    config.debug_level = 0
+    cr_grid = mceq_air.e_grid[30:-10] 
+
+    # ground muon spectrum calculated for given CR energy
+    ground_muspec_prim_energies = []
+
+    for season in tqdm(seasons):  # Loop over seasons
+        season_energies = []  # Temporary container for the current season's results
+        for ei, eprim in enumerate(tqdm(cr_grid)):  # Loop over primary energies
+            theta_energies = np.zeros((thetas.shape[0], mceq_air.dim))
+            if nucleus==2212:
+                mceq_tune.set_single_primary_particle(E=eprim, pdg_id=nucleus)
+            else:
+                mceq_tune.set_single_primary_particle(E=eprim, corsika_id=nucleus)
+            for ia, theta in enumerate(thetas):  # Loop over angles
+                mceq_tune.set_theta_deg(theta)
+                
+                # Optionally set the season-dependent atmospheric model here
+                mceq_tune.density_model.set_season(season)  # Uncomment and adjust if applicable
+                
+                mceq_tune.solve()
+                theta_energies[ia, :] = mceq_tune.get_solution("mu+", mag=0) + mceq_tune.get_solution("mu-", mag=0)
+            
+            season_energies.append(theta_energies)
+        # Append the results for the current season
+        ground_muspec_prim_energies.append(season_energies)
+
+    pickle.dump(
+        [mceq_air.e_grid, cos_thetas, cr_grid, ground_muspec_prim_energies[0], ground_muspec_prim_energies[1], ground_muspec_prim_energies[2]],
+        open("/hetghome/khymon/cs-files/ground_muspec_prim_energies_season_cstune" + str(nucleus) + "_pi" + str(scale_factor_p) + "_k" + str(scale_factor_k) + "_" +str(threshold)+ str(increase)+ ".pkl", "wb"),
+    )
+    pickle.dump(
+        [mceq_air.e_grid, cos_thetas, surface_flux_GSF[0], surface_flux_GSF[1] , surface_flux_GSF[2]],
+        open("/hetghome/khymon/cs-files/surface_fluxes_season" + str(nucleus) + "_pi" + str(scale_factor_p) + "_k" + str(scale_factor_k) + "_" +str(threshold)+ str(increase)+ ".pkl", "wb"),
+    )
+
+    
+if __name__ == '__main__':
+    main()
---
+++ IceCube_diffuse_underground_bundles/old_dev_scripts/Rparameter/R_optimization.py
@@ -0,0 +1,155 @@
+import matplotlib.pyplot as plt
+from matplotlib import cm
+from matplotlib.colors import Normalize
+import matplotlib.lines as mlines
+from scipy.interpolate import UnivariateSpline
+from tqdm import tqdm
+import pickle
+import numpy as np
+
+import crflux.models as pm
+import mceq_underground_helpers_cs_smooth as mh
+
+import click
+
+def X(d):
+    ''' calculate slant depth ad a given vertical depth in km'''
+    s_d = mh.slant_depths
+    angles = mh.angles
+ 
+    return d/np.cos(np.deg2rad(angles))
+
+def dNmu_dmu(d,month, ptype, cs_p1, cs_p2, cs_k2): # month = str
+    '''
+    calculate muon flux per multiplicity
+
+    Params
+    -------
+    d : float
+        Vertical depth in km: 1.5 or 3.5
+    
+    month: string
+        jan, apr or jul
+
+    ptype : int
+        particle id: 2212 = p, 5626 = Fe
+
+    cs : float  > 0
+        Pion-air cross section modification factor. 1 = default (from Sibyll2.3c).
+
+
+    '''
+    angle = 0.0
+
+    #get depth according to angle
+    x_mod = X(d)
+
+    #get number of muons per multiplicity
+    dNmudmu = np.ndarray(shape=(len(x_mod),101),dtype='float')     
+    for i in range(len(x_mod)):
+
+        dNmudmu[i] = 1e4*mh.mult_dist(x_mod[i],
+                                    0.,
+                                    pm.GlobalSplineFitBeta(),"yields_" +month,
+                                    ptype,
+                                    cs_p1, cs_p2,  cs_k2,
+                                    norm=False
+                                ) / mh.rates(x_mod[i], angle, month, ptype, cs_p1, cs_p2,  cs_k2)
+    return dNmudmu
+
+def R(m,dN_dNmu,lower_lim,upper_lim):
+    ''' 
+    calculate R from dN_mu from unmodified and modofied cs  
+
+    Params:
+    ----------
+    m : array
+        multiplicity vector
+    dN_dNmu : array
+        muon flux per multiplicity
+
+    lower_lim/upper_lim : int
+        integtration limits.
+
+        '''
+ 
+    int_low = np.zeros(len(dN_dNmu[:,0]))
+    int_high = np.zeros(len(dN_dNmu[:,0]))
+    for i in range(len(dN_dNmu[:,0])):
+        
+        int_low[i] = np.trapezoid(dN_dNmu[i,1:lower_lim],m[1:lower_lim]) # 19 before
+        int_high[i] = np.trapezoid(dN_dNmu[i,upper_lim:],m[upper_lim:])    #59 before
+
+    return int_high / int_low
+
+def R_normalized(m,R_mod,d,ptype,lower_lim,upper_lim):
+    '''
+        Normalization of R to april as default atmsophere and Sibyll2.3c
+
+    '''
+    # default parameters
+    dNu_dmu_apr = dNmu_dmu(d,month="apr", ptype=ptype, cs_p1=1.0, cs_p2=1.0,  cs_k2=1.0) #default cs
+    R_def_apr = R(m,dNu_dmu_apr,lower_lim,upper_lim)
+    
+    return R_mod/R_def_apr
+
+
+
+
+def main():
+  
+  
+    m = mh.n_mu_vec # muon multiplicity
+   
+
+    #dictionary
+    results = {}  # Dictionary to store the results
+
+    d_values = [1.5, 3.5]# detectpr depth: 1.5 or 3.5km
+    cs_p1_values = [1.05] #[0.5,0.6,0.7,0.8,0.9,1.0,1.1,1.2,1.3,1.4,1.5]  # List of cross-section values: pion-air
+    cs_p2_values = [1.1]
+    cs_k2_values = [1.0]
+    ptype_values = [2212] #, 402, 1608, 5626]  # particle types
+    season_values = ["jan","apr","jul"]  #  seasons
+    lower_lim_values = np.linspace(5,50,45,dtype='int32')
+    upper_lim_values = np.linspace(50,95,45,dtype='int32')
+   
+    for d in d_values:  
+       
+        x_mod = X(d) # for specific depth  
+
+        
+        for cs_p1 in cs_p1_values:
+            for cs_p2 in cs_p2_values:
+                for cs_k2 in cs_k2_values:
+                    for ptype in ptype_values:
+                        for season in season_values:
+                            for ll in lower_lim_values:
+                                for ul in upper_lim_values:
+                                    # Call functions to compute R
+                                    
+                                    dNmu_dmu_mod = dNmu_dmu(d,season, ptype , cs_p1, cs_p2, cs_k2)
+                                    R_mod = R(m,dNmu_dmu_mod, ll, ul)
+                                    R_norm = R_normalized(m,R_mod,d,ptype, ll, ul)
+                                        
+                                    # Store the result in the dictionary
+                                    results[(str(d), str(ll), str(ul), str(cs_p1), str(cs_p2), str(cs_k2), str(ptype), season)] = R_norm.tolist()
+
+    with open("/hetghome/khymon/cs-files/R_value_const_pi-air_sibyll23c_smooth_R_integration_optimizatoin.pkl", "wb") as f:
+        pickle.dump(results, f)
+
+    # Find the key with the maximum R_norm
+    max_key = max(results, key=lambda k: np.max(results[k]))  # Find the key with the highest R_norm value
+    max_R = np.max(results[max_key])  # Get the corresponding max value
+
+
+    print("Maximum R_norm:", max_R)
+    print("Corresponding parameters:", max_key)
+
+
+
+
+if __name__ == '__main__':
+    main()
+
+    
